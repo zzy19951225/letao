89cbd325f09bee839153cd859e111fe344eec152
zzy19951225 <512381961@qq.com>
2018-09-26 20:23:37 +0800

    登录表单校验完成

---
+++ public/back/js/login.js
@@ -0,0 +1,57 @@
+$(function (){
+  $('#form').bootstrapValidator({
+  
+  
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+  
+    //3. 指定校验字段
+    fields: {
+      //校验用户名，对应name表单的name属性
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: '用户名长度必须在2到6之间'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[a-zA-Z0-9_\.]+$/,
+            message: '用户名由数字字母下划线和.组成'
+          }
+        }
+      },
+      password: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '密码名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: '密码长度必须在6到12之间'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[a-zA-Z0-9_\.]+$/,
+            message: '用户名由数字字母下划线和.组成'
+          }
+        }
+      }
+    }
+  
+  })
+ 
+});
