7f79ebf8803056ff51f7848f50f6d27622b64618
zzy19951225 <512381961@qq.com>
2018-09-28 11:34:03 +0800

    首页-柱状图和饼图绘制

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -12,5 +12,40 @@ $(document).ajaxStop(function(){
 
 
 $(function(){
-  
+  $('.lt_aside .category').click(function(){
+    $(".lt_aside .child").stop().slideToggle();
+  });
+
+
+  $('.icon_menu').click(function(){
+    $(".lt_aside ").toggleClass("hidemenu");
+    $(".lt_topbar ").toggleClass("hidemenu");
+    $(".lt_main ").toggleClass("hidemenu");
+  });
+
+
+
+  //退出显示模态框
+  $(".icon_logout").click(function(){
+    $("#logoutModal").modal("show");
+  });
+
+  //模态框退出功能
+  $("#logoutBtn").click(function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      datatype:"json",
+      success:function(info){
+        // console.log(info);
+        if (info.success) {
+         location.href = "login.html";
+        }
+      }
+    })
+  });
+
+
+  //登录拦截
+ 
 })
